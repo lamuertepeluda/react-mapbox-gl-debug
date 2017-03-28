fa7fccf22cb39399b419088435030841b770221a
Vito Macchia <vito.macchia@gmail.com>
2017-03-28 16:46:59 +0200

    Update deps, Remove warning

--- README.md
+++ README.md
@@ -10,7 +10,20 @@ install dependencies:
 npm i
 ```
 
+### Additional Steps
+
+Since this is a fork of [react-mapbox-gl-debug](https://github.com/lamuertepeluda/react-mapbox-gl-debug) which points to [this fork](https://github.com/lamuertepeluda/react-mapbox-gl) of the original [react-mapbox-gl](https://github.com/alex3165/react-mapbox-gl) and **it is not published** on npm, you mus also
+```
+cd node_modules/react-mapbox-gl
+npm i
+npm run build
+```
+
+All the above steps including initial install have bigger success rate using yarn.
+
+## Run the app
 Run the application:
+
 ```
 npm start
 ```
--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react';
 import ReactMap from 'react-mapbox-gl';
 
 const accessToken = "pk.eyJ1IjoiYWxleDMxNjUiLCJhIjoiY2l4b3V0Z3RpMDAxczJ4cWk2YnEzNTVzYSJ9.MFPmOyHy8DM5_CVaqPYhOg";
-const style = "mapbox://styles/mapbox/streets-v9";
+const MAP_STYLES = { street: "mapbox://styles/mapbox/streets-v9" };
 
-const mapStyle = {
-  height: '100vh',
-  width: '100vw'
+const STYLES = {
+  app: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    height: '100vh',
+    width: '100vw',
+    fontFamily: '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif'
+  },
+  container: {
+    position: 'relative',
+    height: '100vh',
+    width: '100vw'
+  }
 }
 
 class App extends Component {
   render() {
     return (
-      <ReactMap
-        style={style}
-        accessToken={accessToken}
-        containerStyle={mapStyle}
-      />
+      <div style={STYLES.app}>
+        <ReactMap
+          style={MAP_STYLES.street}
+          accessToken={accessToken}
+          containerStyle={STYLES.container}
+        />
+      </div>
     );
   }
 }
